fa34d519e9e36ce2802916e3852561f23a8a56be
adson1990 <adson-luks@hotmail.com>
2025-08-12 18:50:08 -0300

    finalizando card e footer da página de receitas, aplicando conceito de static files

--- projeto/settings.py
+++ projeto/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Diretórios adicionais onde o Django vai procurar por arquivos estáticos
+STATICFILES_DIRS = [
+    BASE_DIR / 'recipes/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
